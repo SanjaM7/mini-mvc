c0db6630dc54cf70d319892b14a9f04b01da9627
Sanja Mitrovic <sanja.mitrovic@quantox.com>
2019-11-18 10:21:02 +0100

    [SA-10] fix: test

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 use Application\Core\Application;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 /**
  * MINI - an extremely simple naked PHP application
